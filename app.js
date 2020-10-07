3c3298e05e2a3d0f450c04a369e69632e63f9909
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-10-07 02:39:16 +0100

    new instance initiated of weather class

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // Main application
+
+// Init weather object
+const weather = new Weather('London', 'uk');
+
+weather.getWeather()
+    .then(results => {
+        console.log(results)
+    })
+    .catch(err => console.log(err));
--- weather.js
+++ weather.js
@@ -1,1 +1,26 @@
 // Request to API made with Weather.js
+class Weather {
+    constructor(city, state) {
+        this.apiKey = '09dd78065d824e66b9d03235200710';
+        this.city = city;
+        this.state = state;
+    }
+
+    // Fetch weather from API
+    async getWeather() {
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${this.city}`);
+
+        //http://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${this.city}
+
+        const responseData = await response.json();
+
+        return responseData
+
+    }
+
+    // Change weather location
+    changeLocation(city, state) {
+        this.city = city;
+        this.state = state;
+    }
+}
