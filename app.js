e06af7066680d9679d5be5ea9807df74051f7a5e
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-10-07 23:22:57 +0100

    DOM content load event listener calls getweather

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 // Main application
 
 // Init weather object
-const weather = new Weather('London', 'uk');
+const weather = new Weather('Liverpool', 'uk');
 
-weather.getWeather()
-    .then(results => {
-        console.log(results)
-    })
-    .catch(err => console.log(err));
+//Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather)
+
+// weather.changeLocation('London')
+
+// We wrap weather.getWeather in its own function so it can be called when the DOM loads, so we put an eventListener on it. 
+function getWeather() {
+    weather.getWeather()
+        .then(results => {
+            console.log(results)
+        })
+        .catch(err => console.log(err));
+}
--- weather.js
+++ weather.js
@@ -10,8 +10,6 @@ class Weather {
     async getWeather() {
         const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${this.city}`);
 
-        //http://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${this.city}
-
         const responseData = await response.json();
 
         return responseData
