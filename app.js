924b4a215b6b57bb93d5dee9963771283f366d97
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-10-08 02:10:36 +0100

    inserting get requests results into DOM, and selecting specific index values of weather data

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 // Main application
 
 // Init weather object
-const weather = new Weather('Liverpool', 'uk');
-
+const weather = new Weather('London', 'uk');
+// Initialise UI
+const ui = new UI();
 //Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather)
 
-// weather.changeLocation('London')
+// weather.changeLocation('New York');
 
 // We wrap weather.getWeather in its own function so it can be called when the DOM loads, so we put an eventListener on it. 
 function getWeather() {
     weather.getWeather()
         .then(results => {
+            // instead of just logging the results, we are going to call a method inside the ui class called paint.
             console.log(results)
+            ui.paint(results);// The idea is its 'painting' the ui with the results we pass in. We create this method in the ui.js. 
         })
         .catch(err => console.log(err));
+    
 }
--- ui.js
+++ ui.js
@@ -1,1 +1,27 @@
 // Deals with user interface
+
+class UI {
+    constructor() {
+        this.location = document.getElementById('w-location');
+        this.desc = document.getElementById('w-desc');
+        this.string = document.getElementById('w-string');
+        this.details = document.getElementById('w-details');
+        this.icon = document.getElementById('w-icon');
+        this.humidity = document.getElementById('w-humidity');
+        this.feelsLike = document.getElementById('w-feels-like');
+        this.visMiles = document.getElementById('w-vis-miles');
+        this.wind = document.getElementById('w-wind');
+    }
+
+    paint(weather) { // results are called weather in this case. 
+        this.location.textContent = weather.location.name;
+        this.desc.textContent = weather.current.condition.text;
+        this.string.textContent = weather.current.temp_c;
+        this.icon.setAttribute('src', weather.current.condition.icon);
+        this.humidity.textContent = `Relative Humidity percentage: ${weather.current.humidity}`;
+        this.feelsLike.textContent = `Feels like in celsius: ${weather.current.feelslike_c}`;
+        this.visMiles.textContent = `Visibility in Miles: ${weather.current.vis_miles}`;
+        this.wind.textContent = `Wind in miles per hour: ${weather.current.wind_mph}`;
+
+    }
+}
