d3b630636e72222c2b5fc6e24e9631cfda76f2fa
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-10-09 01:08:29 +0100

    Storage class which uses methods to persist data to local storage to be used again when page reloads as input data for location data inputin ui

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 // Main application
 
+//Init storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
 // Init weather object
-const weather = new Weather('London', 'uk');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+// const weather = new Weather('London', 'UK');
 // Initialise UI
 const ui = new UI();
 //Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather)
 
-// weather.changeLocation('New York');
+  // Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+
+    // Change location
+    weather.changeLocation(city, state);
+
+    // Set location in local storage
+    storage.setLocationData(city, state)
+
+    // Get and display weather
+    getWeather();
+
+    // Close model - bootstraps uses jquery so we use methods from its library below
+    $('#locModal').modal('hide');
+
+
+});
+
+
 
 // We wrap weather.getWeather in its own function so it can be called when the DOM loads, so we put an eventListener on it. 
 function getWeather() {
--- storage.js
+++ storage.js
@@ -1,1 +1,33 @@
 //Local storage in its own file here.
+class Storage {
+    constructor() {
+        this.city;
+        this.state;
+        this.defaultCity = 'London';
+        this.defaultState = 'UK';
+    }
+
+    getLocationData() {
+        if (localStorage.getItem('city') === null) {
+            this.city = this.defaultCity;
+        } else {
+            this.city = localStorage.getItem('city')
+        }
+
+        if (localStorage.getItem('state') === null) {
+            this.state = this.defaultState;
+        } else {
+            this.state = localStorage.getItem('state');
+        }
+
+        return {
+            city: this.city,
+            state: this.state
+        }
+    }
+
+    setLocationData(city, state) {
+        localStorage.setItem('city', city);
+        localStorage.setItem('stat', state);
+    }
+}
